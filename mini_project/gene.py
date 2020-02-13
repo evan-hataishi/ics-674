86548e61ffa2676013a7f825cd9e2d6a8ae4ee48
Evan Hataishi <evanhata@hawaii.edu>
2020-02-13 01:28:53 -1000

    Finish main stuff. Need to add mutation

--- mini_project/gene.py
+++ mini_project/gene.py
@@ -1,21 +1,36 @@
 import random
+import pprint
+from math import sqrt
 
 class Gene():
 
     # right, up, diagonal, none
     moves = ('R', 'U', 'D', None)
 
-    __grid_size = 0
-    __path_length = 0
+    __grid_size  = None
+    __path_length = None
     __path = None
+    __optimal_path = None
+    __optimal_path_points = None
+    __fitness = None
 
-    def __init__(self, GRID_SIZE):
+
+    def __init__(self, GRID_SIZE, path=None):
         self.__grid_size  = GRID_SIZE
         self.__path_length = (GRID_SIZE - 1) * 2
-        self.__path = [random.choice(self.moves) for _ in range(self.__path_length)]
-        self.__path = Gene.restrict_path(self.__path)
-        # self.__optimal_path = ['D' for _ range(self.__path_length)]
-        self.__fitness = -1
+        self.__path = Gene.restrict_path(path) if path else self.__random_path()
+        self.__optimal_path = self.__find_optimal_path()
+        self.__optimal_path_points = self.__find_optimal_path_points()
+        self.__fitness = self.calc_fitness()
+
+
+    # def __init__(self, GRID_SIZE, path):
+    #     self.__grid_size  = GRID_SIZE
+    #     self.__path_length = (GRID_SIZE - 1) * 2
+    #     self.__path = Gene.restrict_path(path)
+    #     self.__optimal_path = self.__find_optimal_path()
+    #     self.__optimal_path_points = self.__find_optimal_path_points()
+    #     self.__fitness = self.calc_fitness()
 
 
     def __str__(self):
@@ -38,6 +53,25 @@ class Gene():
             grid[len(grid[0]) - 1 - y_pos][x_pos] = 'X'
         return grid
 
+
+    @staticmethod
+    def print_grid(grid_size, path):
+        grid = [['_' for _ in range(grid_size)] for _ in range(grid_size)]
+        x_pos, y_pos = 0, 0
+        grid[len(grid[0]) - 1 - y_pos][x_pos] = 'X'
+        for i in range(len(path)):
+            if path[i] == 'R':
+                x_pos += 1
+            elif path[i] == 'U':
+                y_pos += 1
+            elif path[i] == 'D':
+                x_pos += 1
+                y_pos += 1
+            # print(x_pos, y_pos)
+            grid[len(grid[0]) - 1 - y_pos][x_pos] = 'X'
+        pprint.pprint(grid)
+
+
     # It's annoying that I have to check everything before...
     @staticmethod
     def restrict_path(path):
@@ -58,10 +92,11 @@ class Gene():
                 # This step will take us out of the grid. Break out of loop here.
                 break
             step += 1
-        return Gene.__fill_rest(path, grid_size, step, x_pos, y_pos)
+        return Gene.__fill_path(path, grid_size, step, x_pos, y_pos)
+
 
     @staticmethod
-    def __fill_rest(path, grid_size, step, x_pos, y_pos):
+    def __fill_path(path, grid_size, step, x_pos, y_pos):
         while (step < len(path)) and (x_pos < grid_size - 1):
             path[step] = 'R'
             x_pos += 1
@@ -75,8 +110,72 @@ class Gene():
         return path
 
 
-    # def fitness(self):
+    def calc_fitness(self):
+        curr_x, curr_y = 0, 0
+        fitness = 0
+        for i in range(self.__path_length):
+            move = self.__path[i]
+            if move == 'R' or move == 'D':
+                curr_x += 1
+            if move == 'U' or move == 'D':
+                curr_y += 1
+            dist = Gene.__distance((curr_x, curr_y), self.__optimal_path_points[i])
+            fitness += dist
+            # print(dist, (curr_x, curr_y), self.__optimal_path_points[i])
+        return fitness
+
+
+    @staticmethod
+    def __distance(a, b):
+        return sqrt((b[1] - a[1])**2 + (b[0] - a[0])**2)
+
+
+    def __random_path(self):
+        path = [random.choice(self.moves) for _ in range(self.__path_length)]
+        return Gene.restrict_path(path)
+
+
+    def __find_optimal_path(self):
+        optimal_path = [None for _ in range(self.__path_length)]
+        for i in range(self.__grid_size - 1):
+            optimal_path[i] = 'D'
+        return optimal_path
+
+
+    def __find_optimal_path_points(self):
+        final_pos = (self.__grid_size - 1, self.__grid_size - 1)
+        optimal_path = [final_pos for _ in range(self.__path_length)]
+        for i in range(self.__grid_size - 1):
+            optimal_path[i] = (i + 1, i + 1)
+        return optimal_path
 
 
     def get_path(self):
         return self.__path
+
+
+    def get_path_length(self):
+        return self.__path_length
+
+
+    def get_optimal_path(self):
+        return self.__optimal_path
+
+
+    def get_optimal_path_points(self):
+        return self.__optimal_path_points
+
+
+    def get_fitness(self):
+        return self.__fitness
+
+
+    @staticmethod
+    def crossover(p1, p2):
+        length = min(p1.get_path_length(), p2.get_path_length())
+        split = random.randint(0, length)
+        c1_path = p1.get_path()[0:split] + p2.get_path()[split:length]
+        c2_path = p2.get_path()[0:split] + p1.get_path()[split:length]
+        grid_size = (len(c1_path) // 2) + 1
+        # return [Gene(grid_size, c1_path), Gene(grid_size, c2_path)]
+        return [Gene(grid_size, c1_path)]
--- mini_project/main.py
+++ mini_project/main.py
@@ -1,12 +1,60 @@
 from gene import Gene
 import pprint
+import random
 
-GRID_SIZE = 5
+GRID_SIZE = 15
+POPULATION = 100
+GENERATIONS = 1000
+
+def ga():
+    agents = [Gene(GRID_SIZE) for _ in range(POPULATION)]
+    for generation in range(GENERATIONS):
+        print("Generation: %d Population: %d Fitness %.2f" % (generation, len(agents), min([x.get_fitness() for x in agents])))
+        # fitness calculated automatically
+        # selection
+        next_generation = selection(agents)
+        # crossover
+        next_generation += crossover(agents)
+        # mutation
+        # print(min([x.get_fitness() for x in next_generation]))
+        agents = next_generation
+
+        best = min(agents, key=lambda x: x.get_fitness())
+
+        pprint.pprint(best.array_representation())
+
+        if any(agent.get_fitness() == 0 for agent in agents):
+            print("Found optimal!")
+            break
+
+def selection(agents):
+    agents = sorted(agents, key=lambda x: x.get_fitness())
+    s = int((10*POPULATION)/100)
+    return agents[:s]
+
+def crossover(agents):
+    next_generation = []
+    agents = sorted(agents, key=lambda x: x.get_fitness())
+    # random.shuffle(agents)
+    s = int((90*POPULATION)/100)
+    for _ in range(s):
+        p1 = random.choice(agents[:50])
+        p2 = random.choice(agents[:50])
+        next_generation += Gene.crossover(p1, p2)
+    return next_generation
 
 def main():
-    g = Gene(GRID_SIZE)
-    print(str(g))
-    pprint.pprint(g.array_representation())
+    # g = Gene(GRID_SIZE)
+    # print(str(g))
+    # pprint.pprint(g.array_representation())
+    # pprint.pprint(g.array_representation())
+    # Gene.print_grid(GRID_SIZE, g.get_path())
+    # print(g.get_optimal_path())
+    # Gene.print_grid(GRID_SIZE, g.get_optimal_path())
+    # print(g.calc_fitness())
+    ga()
+
+
 
 if __name__ == '__main__':
     main()
