252aec033818db5150d567f48b63905620fca800
Evan Hataishi <evanhata@hawaii.edu>
2020-05-08 00:18:13 -1000

    More code

--- final_project/BST.py
+++ final_project/BST.py
@@ -1,28 +1,66 @@
 import random
 
-class TreeNode():
+class BST():
 
-    key = 0
-    freq = 0
-    left = None
-    right = None
+    root = None
+    # Keep track of nodes by order of insertion
+    nodes = []
 
-    def __init__(self, key, freq):
-        self.key = key
-        self.freq = freq
+    def __init__(self, data):
+        self.insert_nodes(data)
 
     def __str__(self):
-        return "Key: " + str(self.key) + "\tFrequency: " + str(self.freq)
+        return '\n'.join(self.in_order())
 
-    def insert(self, node):
-        if self.key > node.key and self.left:
-            self.left.insert(node)
-        elif self.key > node.key and not self.left:
-            self.left = node
-        elif self.key <= node.key and self.right:
-            self.right.insert(node)
-        else:
-            self.right = node
+    def insert_nodes(self, data):
+        for x in data:
+            node = BST.TreeNode(x[0], x[1])
+            self.nodes.append(node)
+            if not self.root:
+                self.root = node
+            else:
+                self.root.insert(node)
+
+    def in_order(self):
+        nodes = in_order(self.root, 0)
+        return [str(x[0]) + "\tDepth: " + str(x[1]) for x in nodes]
+
+    def traverse(self):
+        traverse(self.root)
+
+    def cost(self):
+        return cost(self.root, 1)
+
+    class TreeNode():
+
+        key = 0
+        freq = 0
+        left = None
+        right = None
+
+        def __init__(self, key, freq):
+            self.key = key
+            self.freq = freq
+
+        def __str__(self):
+            return "Key: " + str(self.key) + "\tFrequency: " + str(self.freq)
+
+        def insert(self, node):
+            if self.key > node.key and self.left:
+                self.left.insert(node)
+            elif self.key > node.key and not self.left:
+                self.left = node
+            elif self.key <= node.key and self.right:
+                self.right.insert(node)
+            else:
+                self.right = node
+
+def in_order(root, depth):
+    if not root:
+        return []
+    left = in_order(root.left, depth+1)
+    right = in_order(root.right, depth+1)
+    return left + [(root, depth)] + right
 
 def traverse(root):
     if not root:
@@ -30,3 +68,10 @@ def traverse(root):
     traverse(root.left)
     print(root)
     traverse(root.right)
+
+def cost(root, level):
+    if not root:
+        return 0
+    left = cost(root.left, level+1)
+    right = cost(root.right, level+1)
+    return (root.freq * level) + left + right
--- final_project/main.py
+++ final_project/main.py
@@ -1,13 +1,46 @@
-from BST import *
+from BST import BST
 import random
+import numpy as np
+
+POP_SIZE = 30
+GENERATIONS = 5
+SELECT_PERC = 0.3
+
+TREE_SIZE = 15
+
+def generate_data():
+    keys = [x for x in range(TREE_SIZE)]
+    freqs = [x for x in range(TREE_SIZE)]
+    return [x for x in zip(keys, freqs)]
+
+def generate_population(data):
+    agents = [None] * POP_SIZE
+    for i in range(POP_SIZE):
+        random.shuffle(data)
+        agents[i] = BST(data)
+    return agents
+
+def ga():
+    data = generate_data()
+    agents = generate_population(data)
+
+    for generation in range(GENERATIONS):
+        print("Generation: %d Population: %d" % (generation, len(agents)))
+
+        fitness = [- x.cost() for x in agents]
+
+        stat = (max(fitness), np.mean(fitness), min(fitness))
+
+        print("Best: %.2f Avg: %.2f Worst: %.2f" % stat)
 
 def main():
-    root = TreeNode(1, 0)
-    keys = [x for x in range(2, 50)]
-    random.shuffle(keys)
-    for key in keys:
-        root.insert(TreeNode(key, 0))
-    traverse(root)
+    # data = generate_data()
+    # # Shuffle here
+    # print(data)
+    # t = BST(data)
+    # print(t)
+    # print(t.cost())
+    ga()
 
 if __name__ == '__main__':
     main()
